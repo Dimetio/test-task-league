d9b618c9a703ab895fc064ec284f53b3d751c519
ddubinin <dubinin.dmitry@mail.ru>
2023-04-13 20:35:19 +0300

    feat: create store use effector

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import "./App.css";
-import {
-  Routes,
-  Route,
-  useLocation,
-  useNavigate,
-  Link,
-} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Input from "./components/input/input";
 import { Answer, Info } from "./pages/index";
 
 function App() {
-
   return (
     <div className="App">
       <section className="section">
-        <Link to="/">main</Link>
-        <Link to="/answer">answer</Link>
-        <Link to="/info">info</Link>
         <Routes>
           <Route path="answer" element={<Answer />} />
           <Route path="info" element={<Info />} />

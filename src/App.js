0946f905d14edfcbdb9ee789ac4e146deae2387c
ddubinin <dubinin.dmitry@mail.ru>
2023-04-13 13:02:39 +0300

    feat: added routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import "./App.css";
+import {
+  Routes,
+  Route,
+  useLocation,
+  useNavigate,
+  Link,
+} from "react-router-dom";
 import Input from "./components/input/input";
-import Table from "./components/table/table";
+import { Answer, Info } from "./pages/index";
 
 function App() {
   return (
     <div className="App">
       <section className="section">
-        <Input />
-        <Table />
+        <Link to="/">main</Link>
+        <Link to="/answer">answer</Link>
+        <Link to="/info">info</Link>
+        <Routes>
+          <Route path="answer" element={<Answer />} />
+          <Route path="info" element={<Info />} />
+          <Route path="/" element={<Input />} />
+        </Routes>
       </section>
     </div>
   );
---
+++ src/pages/index.jsx
@@ -0,0 +1,4 @@
+import Answer from "./answer/answer";
+import Info from "./info/info";
+
+export { Answer, Info };

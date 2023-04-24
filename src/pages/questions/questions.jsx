81fb32b9f0637c64d574f09058df968169620b1d
ddubinin <dubinin.dmitry@mail.ru>
2023-04-25 01:40:36 +0300

    fix: use model on store

--- src/pages/questions/questions.jsx
+++ src/pages/questions/questions.jsx
@@ -1,10 +1,10 @@
 import styles from "./questions.module.css";
 import { useStore } from "effector-react";
-import { $search } from "../../components/input/input";
+import { modelSearch } from "../../models/search";
 import Table from "../../components/table/table";
 
 export default function Questions() {
-  const searchStore = useStore($search);
+  const searchStore = useStore(modelSearch.$search);
   return (
     searchStore && (
       <Table title="Данные по запросу в поиске" items={searchStore} />
